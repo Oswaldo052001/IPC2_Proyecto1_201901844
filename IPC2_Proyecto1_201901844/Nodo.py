2282928a0910ba3e986882738c57e183630b8383
Oswaldo052001 <3014167370101@ingenieria.usac.edu.gt>
2023-08-19 23:37:28 -0600

    Se creó las funciones de las clases y la inserción de los datos

--- IPC2_Proyecto1_201901844/Nodo.py
+++ IPC2_Proyecto1_201901844/Nodo.py
@@ -1,8 +1,16 @@
+
+
 class Nodo():
-    def __init__(self, dato):
+    def __init__(self,id, dato):
+        self.id = id
         self.dato = dato
         self.siguiente = None
-        
+
+    def getId(self):
+        return self.id
+    
+    def setId(self, id):
+        self.id = id
 
     def getDato(self):
         return self.dato
