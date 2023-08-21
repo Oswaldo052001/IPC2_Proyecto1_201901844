9504df5327ab75bab463a993bc79698c4fe6f330
Oswaldo052001 <3014167370101@ingenieria.usac.edu.gt>
2023-08-20 22:26:04 -0600

    Se creó setDato para almacenar los datos del xml

--- IPC2_Proyecto1_201901844/Amplitud.py
+++ IPC2_Proyecto1_201901844/Amplitud.py
@@ -6,6 +6,7 @@ class Amplitud:
     def __init__(self, amplitud, dato = 0):
         self.amplitud = amplitud
         self.dato = dato
+        self.imprimir()
 
     def getAmplitud(self):
         return self.amplitud
@@ -13,5 +14,8 @@ class Amplitud:
     def getdato(self):
         return self.dato
     
+    def setdato(self,dato):
+        self.dato = dato
+
     def imprimir(self):
         print(self.amplitud, self.dato)
