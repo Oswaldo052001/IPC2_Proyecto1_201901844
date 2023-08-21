d0e88c957a6f02d31e4b7d714f82b5a94a131965
Oswaldo052001 <3014167370101@ingenieria.usac.edu.gt>
2023-08-20 22:27:06 -0600

    Se realizo comentarios y la función AgregarDato

--- IPC2_Proyecto1_201901844/DocXML.py
+++ IPC2_Proyecto1_201901844/DocXML.py
@@ -14,19 +14,26 @@ class xml():
 
         listSenales = Listaenlazada()
 
-        for senal in self.senales.findall('senal'):
-            nombre = senal.get('nombre')
-            tiempoMax = senal.get("t")
-            amplitudMax = senal.get("A")
-            tmpSenal = Senal(nombre, tiempoMax, amplitudMax)
-            listSenales.agregarFinal(tmpSenal)
-        
+        for senal in self.senales.findall('senal'):    # este for va a recorer todas las senales que esten en el doc
+            nombre = senal.get('nombre')  # Obteniendo nombre de la senal
+            tiempoMax = senal.get("t")    # Obteniendo tiempo de la senal
+            amplitudMax = senal.get("A")  # Obteniendo amplitud de la senal
+
+            tmpSenal = Senal(nombre, tiempoMax, amplitudMax)   ## Creando las senalas que hay en el xml
+            listSenales.agregarFinal(tmpSenal)  # Agreando las senales a la lista senales
+
+
+
+        # Imprimiendo lista senales
+               
         print("--------- Lista de senales----------")
         senalGuardada = listSenales.getInicio()
         while senalGuardada != None:
-            print(senalGuardada.getDato().getNombre())
+            self.IngresandoDatos(senalGuardada.getDato())     # Aguardando los datos de cada senal
+            #print(senalGuardada.getDato().getNombre()) 
             senalGuardada = senalGuardada.getSiguiente()
             
+
         #print("nombre: "+nombre)
         #print("tiempo maximo: "+tiempoMax)
         #print("amplitud maxima: "+amplitudMax)
@@ -34,6 +41,41 @@ class xml():
         #self.VerificarRepetidos(senal)
         #self.imprimir(senal)
         
+
+            
+    def IngresandoDatos(self,SenalGuardada):    
+
+        lstTiempo = SenalGuardada.ListaTiempos   # Variable que almacena la lista de Tiempos
+        lstAmplitud = lstTiempo.getInicio().getDato().ListaAmplitudes   # Variable que almacena la lsita de amplitudes
+    
+        for senal in self.senales.findall('senal'):        # Recorer cada senal con los datos que tiene el xml
+            nombre = senal.get('nombre')                   
+            if SenalGuardada.getNombre() ==  nombre:       # Validando para que se ingresen en los datos de la senal por el nombre
+                for datos in senal:                        # Recorriendo los datos que tiene cada senal
+                    tiempo = datos.get("t")                # tiempo       
+                    amplitud = datos.get("A")              # Amplitud
+                    valor= datos.text                      # Valor
+
+                    print("tiempo = "+tiempo+ " amplitud ="+amplitud+ " Valor ="+str(valor))
+                   
+                    objtiempo = lstTiempo.getInicio()     # Obteniendo el nodo inicial de la lista de tiempos              
+                    while objtiempo != None:
+                        objamplitud = lstAmplitud.getInicio()     # Obteniendo el nodo inicial de la lista de amplitudes
+                        while objamplitud != None:                # Este while va a recorer cada amplitud de cada tiempo 
+                            if int(objtiempo.getDato().getTiempo()) == int(tiempo) and int(objamplitud.getDato().getAmplitud()) == int(amplitud):   #Verificando si cumple con el tiempo y la amplitud del archivo con el que tiene cada lista 
+                                objamplitud.getDato().setdato(valor)   # Seteando el valor del dato al atributo de amplitud
+                                objamplitud.getDato().imprimir()       
+
+                            objamplitud = objamplitud.getSiguiente()   # Si no cumple ir cambiando de nodo
+                        objtiempo = objtiempo.getSiguiente()           # Si no cumple ir cambiando de nodo
+
+                    
+                    
+            else:
+                print("no existe")   # Si el nombre de la tabla ingresada no esta registrada en ListaSenales 
+            
+
+  
     def VerificarRepetidos(self,senal):
 
         for datos in senal:
@@ -50,11 +92,7 @@ class xml():
                     self.contador -= 1
 
                 self.datoguardado = valor
-            
-    def getDatos(self,senal):     ##PENDIENTE PARA SET LOS DATOS 
-        for datos in senal:
-            pass
-  
+
 
     def imprimir(self, senal):
         for datos in senal:
