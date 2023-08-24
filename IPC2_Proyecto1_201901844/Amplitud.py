45fec6b2e06de9a5068c5a089c207a105e5c27cf
Oswaldo052001 <3014167370101@ingenieria.usac.edu.gt>
2023-08-23 23:01:25 -0600

    Se insertaron nuevos atributos para la clase amplitud

--- IPC2_Proyecto1_201901844/Amplitud.py
+++ IPC2_Proyecto1_201901844/Amplitud.py
@@ -3,19 +3,27 @@
 
 class Amplitud:
 
-    def __init__(self, amplitud, dato = 0):
+    def __init__(self, amplitud, dato = 0, valorBinario = 0):
         self.amplitud = amplitud
         self.dato = dato
-        self.imprimir()
+        self.valorBinario = valorBinario
+        #self.imprimir()
+    
 
     def getAmplitud(self):
         return self.amplitud
     
-    def getdato(self):
+    def getValor(self):
         return self.dato
     
-    def setdato(self,dato):
+    def setValor(self,dato):
         self.dato = dato
+    
+    def getBinario(self):
+        return self.valorBinario
+    
+    def setBinario(self,binario):
+        self.valorBinario = binario
 
     def imprimir(self):
         print(self.amplitud, self.dato)
